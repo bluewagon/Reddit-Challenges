b4cd5658d8b1d7a5b0223bae947883c0fe7ddd4a
Austen Jennings <austenhjennings@gmail.com>
2016-09-13 23:45:28 -0400

    added ConsoleInput

---
+++ Easy/Challenge 1/csharp/UserInformation/UserInformation/ConsoleInput.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace UserInformation
+{
+    public class ConsoleInput
+    {
+        public string GetName()
+        {
+            return GetString("Please enter your name: ");
+        }
+
+        public int GetAge()
+        {
+            int age = 0;
+            while (age < 1 || age > 99)
+            {
+                Console.WriteLine("Please enter your age: ");
+                var inputAge = Console.ReadLine();
+                if (Int32.TryParse(inputAge, out age))
+                {
+                    if (age < 1 || age > 99)
+                    {
+                        Console.WriteLine("Age is not valid.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please use a number.");
+                }
+            }
+            return age;
+        }
+
+        public string GetUsername()
+        {
+            return GetString("Please enter your username: ");
+        }
+
+        private string GetString(string message)
+        {
+            string item = string.Empty;
+            while (string.IsNullOrEmpty(item))
+            {
+                Console.WriteLine(message);
+                item = Console.ReadLine();
+            }
+            return item;
+        }
+    }
+}
---
+++ Easy/Challenge 1/csharp/UserInformation/UserInformationTests/ConsoleInputTests.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserInformation;
+
+namespace UserInformationTests
+{
+    [TestClass]
+    public class ConsoleInputTests
+    {
+        private ConsoleInput _consoleInput;
+        private string _name;
+        private int _age;
+        private string _username;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _consoleInput = new ConsoleInput();
+            _name = "John";
+            _age = 24;
+            _username = "bluewagon";
+        }
+
+        [TestMethod]
+        public void ConsoleInput_ShouldAskForNameOnce()
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                using (StringReader reader = new StringReader($"{_name}{Environment.NewLine}"))
+                {
+                    Console.SetIn(reader);
+
+                    string inputName = _consoleInput.GetName();
+                    Assert.AreEqual(_name, inputName);
+
+                    string output = writer.ToString();
+                    string expected = $"Please enter your name: {Environment.NewLine}";
+                    Assert.AreEqual(expected, output);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ConsoleInput_ShouldAskAgainIfNameIsEmpty()
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                using (StringReader reader = new StringReader($"{string.Empty}{Environment.NewLine}{_name}{Environment.NewLine}"))
+                {
+                    Console.SetIn(reader);
+
+                    string inputName = _consoleInput.GetName();
+                    Assert.AreEqual(_name, inputName);
+
+                    string output = writer.ToString();
+                    string expected = $"Please enter your name: {Environment.NewLine}Please enter your name: {Environment.NewLine}";
+                    Assert.AreEqual(expected, output);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ConsoleInput_ShouldAskForAgeOnce()
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                using (StringReader reader = new StringReader($"{_age}{Environment.NewLine}"))
+                {
+                    Console.SetIn(reader);
+
+                    int inputAge = _consoleInput.GetAge();
+                    Assert.AreEqual(_age, inputAge);
+
+                    string output = writer.ToString();
+                    string expected = $"Please enter your age: {Environment.NewLine}";
+                    Assert.AreEqual(expected, output);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ConsoleInput_ShouldAskForAgeAgainIfNull()
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                using (StringReader reader = new StringReader($"{string.Empty}{Environment.NewLine}{_age}{Environment.NewLine}"))
+                {
+                    Console.SetIn(reader);
+
+                    int inputAge = _consoleInput.GetAge();
+                    Assert.AreEqual(_age, inputAge);
+
+                    string output = writer.ToString();
+                    string expected = $"Please enter your age: {Environment.NewLine}Please use a number." +
+                                      $"{Environment.NewLine}Please enter your age: {Environment.NewLine}";
+                    Assert.AreEqual(expected, output);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ConsoleInput_ShouldAskForAgeAgainIfLessThanOne()
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                using (StringReader reader = new StringReader($"0{Environment.NewLine}{_age}{Environment.NewLine}"))
+                {
+                    Console.SetIn(reader);
+
+                    int inputAge = _consoleInput.GetAge();
+                    Assert.AreEqual(_age, inputAge);
+
+                    string output = writer.ToString();
+                    string expected = $"Please enter your age: {Environment.NewLine}Age is not valid." +
+                                      $"{Environment.NewLine}Please enter your age: {Environment.NewLine}";
+                    Assert.AreEqual(expected, output);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ConsoleInput_ShouldAskForAgeAgainIfGreaterThan100()
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                using (StringReader reader = new StringReader($"100{Environment.NewLine}{_age}{Environment.NewLine}"))
+                {
+                    Console.SetIn(reader);
+
+                    int inputAge = _consoleInput.GetAge();
+                    Assert.AreEqual(_age, inputAge);
+
+                    string output = writer.ToString();
+                    string expected = $"Please enter your age: {Environment.NewLine}Age is not valid." +
+                                      $"{Environment.NewLine}Please enter your age: {Environment.NewLine}";
+                    Assert.AreEqual(expected, output);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ConsoleInput_ShouldAskForUsernameOnce()
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                using (StringReader reader = new StringReader($"{_username}{Environment.NewLine}"))
+                {
+                    Console.SetIn(reader);
+
+                    string inputUsername = _consoleInput.GetUsername();
+                    Assert.AreEqual(_username, inputUsername);
+
+                    string output = writer.ToString();
+                    string expected = $"Please enter your username: {Environment.NewLine}";
+                    Assert.AreEqual(expected, output);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ConsoleInput_ShouldAskAgainIfUsernameIsEmpty()
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                using (StringReader reader = new StringReader($"{string.Empty}{Environment.NewLine}{_username}{Environment.NewLine}"))
+                {
+                    Console.SetIn(reader);
+
+                    string inputUsername = _consoleInput.GetUsername();
+                    Assert.AreEqual(_username, inputUsername);
+
+                    string output = writer.ToString();
+                    string expected = $"Please enter your username: {Environment.NewLine}Please enter your username: {Environment.NewLine}";
+                    Assert.AreEqual(expected, output);
+                }
+            }
+        }
+    }
+}
